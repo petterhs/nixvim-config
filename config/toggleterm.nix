8dd6f66ea137bb46b8d54cfea09a0ab33dc865a0
Petter Hoem Sletsjøe <petter.hoem.sletsjoe@semcon.com>
2024-05-04 00:09:45 +0200

    toggleterm: resize term

--- config/toggleterm.nix
+++ config/toggleterm.nix
@@ -1,6 +1,12 @@
 {pkgs, ... }:
 {
-  plugins.toggleterm.enable = true;
+  plugins.toggleterm = {
+    enable = true;
+    settings.float_opts = {
+      height = 20;
+      width = 60;
+    };
+  };
   keymaps = [
     {
       key = "<A-i>";
