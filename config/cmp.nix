8bfaf8ebbca78d93e4da2871e3bc126123a03c26
Petter Hoem Sletsjøe <petter.hoem.sletsjoe@semcon.com>
2024-05-14 17:11:31 +0200

    fix completions

--- config/cmp.nix
+++ config/cmp.nix
@@ -1,10 +1,57 @@
-{pkgs, ... }:
 {
+  opts.completeopt = ["menu" "menuone" "noselect"];
+
   plugins = {
-    cmp.enable = true;
-    cmp-nvim-lsp.enable = true;
-    cmp-buffer.enable = true;
-    cmp-path.enable = true;
-    cmp-treesitter.enable = true;
+
+    luasnip.enable = true;
+
+    lspkind = {
+      enable = true;
+      cmp = {
+        enable = true;
+        menu = {
+          nvim_lsp = "[LSP]";
+          nvim_lua = "[api]";
+          path = "[path]";
+          luasnip = "[snip]";
+          buffer = "[buffer]";
+          copilot = "[AI]";
+        };
+      };
+    };
+
+    copilot-lua = {
+      enable = true;
+      panel.enabled = false;
+      suggestion.enabled = false;
+    };
+
+    cmp = {
+      enable = true;
+      settings = {
+
+        snippet.expand = "luasnip";
+
+        mapping = {
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-e>" = "cmp.mapping.close()";
+          "<C-j>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+          "<C-k>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+        };
+
+        sources = [
+          {name = "path";}
+          {name = "nvim_lsp";}
+          {name = "luasnip";}
+          {name = "copilot";}
+          {
+            name = "buffer";
+            # Words from other open buffers can also be suggested.
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+          }
+        ];
+      };
+    };
   };
 }
